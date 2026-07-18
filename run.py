6f6c55e159cfd549f08a8bb21577b86a6337ba48
Matt Birum <45514707+mbirum@users.noreply.github.com>
2026-07-18 17:55:31 -0400

    Adjust sleep interval and rotation for motor control
    Reduced sleep interval for faster motor control and increased rotation count.

--- run.py
+++ run.py
@@ -11,13 +11,13 @@ GPIO.setmode(GPIO.BOARD)
 
 left_pins = [7,11,13,15]
 right_pins = [11,13,15,37]
-sleep_interval = 0.001
+sleep_interval = 0.0001
 
 control_pins = right_pins
 sequence = motor_sequencer.forward()
 
 # max 512
-rotation = 3
+rotation = 10
 
 # initialize pins
 for pin in control_pins:
@@ -81,11 +81,11 @@ else:
 				for step in range(len(sequence)):
 					for pin in range(4):
 						GPIO.output(control_pins[pin], sequence[step][pin])
-					#time.sleep(sleep_interval)
+					time.sleep(sleep_interval)
 		elif y_axis_direction > 0:
 			sequence = motor_sequencer.backward()
 			for i in range(int(rotation)):
 				for step in range(len(sequence)):
 					for pin in range(4):
 						GPIO.output(control_pins[pin], sequence[step][pin])
-					#time.sleep(sleep_interval)
+					time.sleep(sleep_interval)
