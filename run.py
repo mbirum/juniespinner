e05a9e4773201e6ac150837ded8547c8ed21aaf4
Matt Birum <45514707+mbirum@users.noreply.github.com>
2026-07-18 17:49:23 -0400

    Comment out time.sleep for motor control
    Comment out sleep interval in motor control loop.

--- run.py
+++ run.py
@@ -81,11 +81,11 @@ else:
 				for step in range(len(sequence)):
 					for pin in range(4):
 						GPIO.output(control_pins[pin], sequence[step][pin])
-					time.sleep(sleep_interval)
+					#time.sleep(sleep_interval)
 		elif y_axis_direction > 0:
 			sequence = motor_sequencer.backward()
 			for i in range(int(rotation)):
 				for step in range(len(sequence)):
 					for pin in range(4):
 						GPIO.output(control_pins[pin], sequence[step][pin])
-					time.sleep(sleep_interval)
+					#time.sleep(sleep_interval)
