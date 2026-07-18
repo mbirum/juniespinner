49f0266f5319029268487596cb3d357b3a5498e8
Matt Birum <45514707+mbirum@users.noreply.github.com>
2026-07-18 17:18:42 -0400

    Implement forward and backward motor sequences

---
+++ motor_sequencer.py
@@ -0,0 +1,21 @@
+forward_sequence = [
+  [1,0,0,0],
+  [1,1,0,0],
+  [0,1,0,0],
+  [0,1,1,0],
+  [0,0,1,0],
+  [0,0,1,1],
+  [0,0,0,1],
+  [1,0,0,1]
+]
+
+def forward():
+  return forward_sequence
+
+def backward():
+  backward_sequence = []
+  last_index = len(forward_sequence) - 1
+  for i in range(len(forward_sequence)):
+    index = last_index - i
+    backward_sequence.append(forward_sequence[index])
+  return backward_sequence
